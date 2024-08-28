6b2bc51840a3ef4589fb86cfeb7dd849b6859c82
OYEDUNMOLA230329 <alaooyedunmola2022@gmail.com>
2024-08-28 13:34:47 +0100

    Add files via upload

---
+++ NameAndMatric.java
@@ -0,0 +1,18 @@
+package name.and.matric;
+
+
+public class NameAndMatric {
+
+    
+    public static void main(String[] args) {
+    
+        String name = "Olawiwola Imoleayo Itunu";
+        String Matricnumber ="ND/230423";
+        
+        System.out.println("Name:" + name);
+        System.out.println("Matric Number:" + Matricnumber);
+        
+        
+    }
+    
+}
